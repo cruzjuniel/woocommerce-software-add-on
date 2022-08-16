169075294ede863a261beb4237fa14ee6e57aed0
Juniel Cruz <cruz.juniel@gmail.com>
2022-08-17 07:36:20 +0800

    Exported checkLicense and generateKey as functions

--- lib/index.ts
+++ lib/index.ts
@@ -7,6 +7,123 @@ import {
     WooCommerceSoftwareCheckFailed
 } from "./types";
 
+async function getRequest(hostname: string, request: string, args: { [key: string]: string | null }): Promise<WooCommerceSoftwareResult> {
+    let requestPath: string = "/woocommerce/?wc-api=software-api"
+    requestPath += `&request=${request}`
+
+    Object.keys(args).forEach(key => {
+        if (args[key]) {
+            requestPath += `&${key}=${args[key]}`
+        }
+    });
+
+    return await new Promise<WooCommerceSoftwareResult>(resolve => {
+        https.get({
+            hostname: hostname,
+            path: requestPath
+        }, (res) => {
+
+            if (res.statusCode != undefined && res.statusCode == 200) {
+
+                let data: string = "";
+
+                // A chunk of data has been received.
+                res.on('data', (d) => {
+                    data += d;
+                });
+
+                // The whole response has been received. Print out the result.
+                res.on('end', () => {
+
+                    let json: {} = {};
+
+                    try {
+                        json = JSON.parse(data);
+                    } catch (e) {
+                        resolve({
+                            success: false,
+                            code: res.statusCode,
+                            headers: res.headers,
+                            output: data,
+                            error: e
+                        });
+                    }
+
+                    resolve({
+                        success: true,
+                        code: res.statusCode,
+                        headers: res.headers,
+                        output: json,
+                        error: "No error occurred"
+                    });
+
+                });
+
+            } else {
+
+                resolve({
+                    success: false,
+                    code: res.statusCode,
+                    headers: res.headers,
+                    output: null,
+                    error: `Status code is ${res.statusCode}`
+                });
+
+            }
+
+        }).on('error', (e) => {
+
+            resolve({
+                success: false,
+                code: 0,
+                headers: null,
+                output: null,
+                error: e
+            });
+
+        });
+    });
+}
+
+// generate_key: https://woocommerce.com/document/software-add-on/#section-10
+export async function generateKey(hostname: string, product_id: string, email: string, secret_key: string, order_id: string | null = null, version: string | null = null, key_prefix: string | null = null, activations: number = 1): Promise<WooCommerceSoftwareResult> {
+    return await getRequest(hostname, "generate_key", {
+        secret_key: secret_key,
+        email: email,
+        product_id: product_id,
+        order_id: order_id,
+        version: version,
+        key_prefix: key_prefix,
+        activations: String(activations)
+    });
+}
+
+// check: https://woocommerce.com/document/software-add-on/#section-14
+export async function checkLicense(hostname: string, product_id: string, email: string, license_key: string, timestamp: string | number = 0, platform: string = ""): Promise<WooCommerceSoftwareResult> {
+    let result: WooCommerceSoftwareResult = await getRequest(hostname, "check", {
+        email: email,
+        license_key: license_key,
+        product_id: product_id
+    });
+    if (result.success) {
+        let output: WooCommerceSoftwareCheckResult = (result.output as WooCommerceSoftwareCheckResult);
+        if (!output.success) {
+            result.success = false;
+            result.error = `Code ${(output as WooCommerceSoftwareCheckFailed).code}: ${(output as WooCommerceSoftwareCheckFailed).error}`;
+        } else {
+            if ((output as WooCommerceSoftwareCheckSuccess).activations.find(activation => {
+                // if timestamp is 0 (no timestamp was provided), instance is ignored
+                // if platform is empty string (no platform was provided), activation_platform is ignored
+                return (timestamp == 0 || activation.instance == String(timestamp)) && (platform == "" || platform == activation.activation_platform);
+            }) === undefined) {
+                result.success = false;
+                result.error = `No matching timestamp '${timestamp}' and/or platform '${platform}'`;
+            }
+        }
+    }
+    return result;
+}
+
 export class WooCommerceSoftwareAddOn {
     hostname: string;
     product_id: string;
@@ -18,21 +135,15 @@ export class WooCommerceSoftwareAddOn {
         this.email = email;
     }
 
-    // generate_key: https://woocommerce.com/document/software-add-on/#section-10
-    async generateKey(secret_key: string, order_id: string | null = null, version: string | null = null, key_prefix: string | null = null, activations: number = 1): Promise<WooCommerceSoftwareResult>{
-        return await this.getRequest("generate_key", {
-            secret_key: secret_key,
-            email: this.email,
-            product_id: this.product_id,
-            order_id: order_id,
-            version: version,
-            key_prefix: key_prefix,
-            activations: String(activations)
-        });
-    }
-
     // activation: https://woocommerce.com/document/software-add-on/#section-11
-    async activateLicense(license_key: string, instance: string | null = null, platform: string | null = null, secret_key: string | null = null): Promise<WooCommerceSoftwareResult>{
+    async activateLicense(license_key: string, instance: string | null = null, platform: string | null = null, secret_key: string | null = null): Promise<WooCommerceSoftwareResult> {
+        if (this.email == null) return {
+            success: false,
+            code: undefined,
+            headers: null,
+            output: null,
+            error: "No email set"
+        }
         return await this.getRequest("activation", {
             email: this.email,
             license_key: license_key,
@@ -44,7 +155,14 @@ export class WooCommerceSoftwareAddOn {
     }
 
     // activation_reset: https://woocommerce.com/document/software-add-on/#section-12
-    async resetLicense(license_key: string): Promise<WooCommerceSoftwareResult>{
+    async resetLicense(license_key: string): Promise<WooCommerceSoftwareResult> {
+        if (this.email == null) return {
+            success: false,
+            code: undefined,
+            headers: null,
+            output: null,
+            error: "No email set"
+        }
         return await this.getRequest("activation_reset", {
             email: this.email,
             license_key: license_key,
@@ -53,7 +171,14 @@ export class WooCommerceSoftwareAddOn {
     }
 
     // deactivation: https://woocommerce.com/document/software-add-on/#section-13
-    async deactivateLicense(license_key: string, instance: string | null = null, activation_id: string | null = null): Promise<WooCommerceSoftwareResult>{
+    async deactivateLicense(license_key: string, instance: string | null = null, activation_id: string | null = null): Promise<WooCommerceSoftwareResult> {
+        if (this.email == null) return {
+            success: false,
+            code: undefined,
+            headers: null,
+            output: null,
+            error: "No email set"
+        }
         return await this.getRequest("deactivation", {
             email: this.email,
             license_key: license_key,
@@ -64,107 +189,18 @@ export class WooCommerceSoftwareAddOn {
     }
 
     // check: https://woocommerce.com/document/software-add-on/#section-14
-    async checkLicense(license_key: string, timestamp: string | number = 0, platform: string = ""): Promise<WooCommerceSoftwareResult>{
-        let result : WooCommerceSoftwareResult = await this.getRequest("check", {
-            email: this.email,
-            license_key: license_key,
-            product_id: this.product_id
-        });
-        if (result.success) {
-            let output : WooCommerceSoftwareCheckResult = (result.output as WooCommerceSoftwareCheckResult);
-            if (!output.success) {
-                result.success = false;
-                result.error = `Code ${(output as WooCommerceSoftwareCheckFailed).code}: ${(output as WooCommerceSoftwareCheckFailed).error}`;
-            } else {
-                if ((output as WooCommerceSoftwareCheckSuccess).activations.find(activation => {
-                    // if timestamp is 0 (no timestamp was provided), instance is ignored
-                    // if platform is empty string (no platform was provided), activation_platform is ignored
-                    return (timestamp == 0 || activation.instance == String(timestamp)) && (platform == "" || platform == activation.activation_platform);
-                }) === undefined) {
-                    result.success = false;
-                    result.error = `No matching timestamp '${timestamp}' and/or platform '${platform}'`;
-                }
-            }
+    async checkLicense(license_key: string, timestamp: string | number = 0, platform: string = ""): Promise<WooCommerceSoftwareResult> {
+        if (this.email == null) return {
+            success: false,
+            code: undefined,
+            headers: null,
+            output: null,
+            error: "No email set"
         }
-        return result;
+        return checkLicense(this.hostname, this.product_id, this.email, license_key, timestamp, platform);        
     }
 
-    private async getRequest(request: string, args: { [key: string]: string | null }): Promise<WooCommerceSoftwareResult>{
-        let requestPath: string = "/woocommerce/?wc-api=software-api"
-        requestPath += `&request=${request}`
-
-        Object.keys(args).forEach(key => {
-            if (args[key]) {
-                requestPath += `&${key}=${args[key]}`
-            }
-        });
-
-        return await new Promise<WooCommerceSoftwareResult>(resolve => {
-            https.get({
-                hostname: this.hostname,
-                path: requestPath
-            }, (res) => {
-
-                if (res.statusCode != undefined && res.statusCode == 200) {
-                    
-                    let data: string = "";
-
-                    // A chunk of data has been received.
-                    res.on('data', (d) => {
-                        data += d;
-                    });
-
-                    // The whole response has been received. Print out the result.
-                    res.on('end', () => {
-
-                        let json : {} = {};
-                        
-                        try {
-                            json = JSON.parse(data);
-                        } catch (e) {
-                            resolve({
-                                success: false,
-                                code: res.statusCode,
-                                headers: res.headers,
-                                output: data,
-                                error: e
-                            });
-                        }
-
-                        resolve({
-                            success: true,
-                            code: res.statusCode,
-                            headers: res.headers,
-                            output: json,
-                            error: "No error occurred"
-                        });
-
-                    });
-                    
-                } else {
-
-                    resolve({
-                        success: false,
-                        code: res.statusCode,
-                        headers: res.headers,
-                        output: null,                        
-                        error: `Status code is ${res.statusCode}`
-                    });
-
-                }
-
-            }).on('error', (e) => {
-
-                resolve({
-                    success: false,
-                    code: 0,
-                    headers: null,
-                    output: null,                        
-                    error: e
-                });
-
-            });
-        });
-
+    private async getRequest(request: string, args: { [key: string]: string | null }): Promise<WooCommerceSoftwareResult> {
+        return getRequest(this.hostname, request, args);
     }
 }
