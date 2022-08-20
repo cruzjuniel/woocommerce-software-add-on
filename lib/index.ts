cf3912a4731ae4abd0ccda990bea2cbfaf9b67bb
Juniel Cruz <cruz.juniel@gmail.com>
2022-08-20 10:28:03 +0800

    Added auto evaluation of API errors

--- lib/index.ts
+++ lib/index.ts
@@ -2,11 +2,18 @@
 import https = require('https');
 import {
     WooCommerceSoftwareResult,
-    WooCommerceSoftwareCheckResult,
     WooCommerceSoftwareCheckSuccess,
-    WooCommerceSoftwareCheckFailed,
-    WooCommerceSoftwareActivations
+    WooCommerceSoftwareActivations,
+    WooCommerceSoftwareApiError
 } from "./types";
+
+const noEmailError: WooCommerceSoftwareResult = {
+    success: false,
+    code: undefined,
+    headers: null,
+    output: null,
+    error: "No email set"
+};
 
 async function getRequest(hostname: string, request: string, args: { [key: string]: string | null }): Promise<WooCommerceSoftwareResult> {
     let requestPath: string = "/woocommerce/?";
@@ -26,7 +33,7 @@ async function getRequest(hostname: string, request: string, args: { [key: strin
             path: requestPath
         }, (res) => {
 
-            if (res.statusCode != undefined && res.statusCode == 200) {
+            if (res.statusCode !== undefined && res.statusCode == 200) {
 
                 let data: string = "";
 
@@ -38,7 +45,7 @@ async function getRequest(hostname: string, request: string, args: { [key: strin
                 // The whole response has been received. Print out the result.
                 res.on('end', () => {
 
-                    let json: {} = {};
+                    let json = {};
 
                     try {
                         json = JSON.parse(data);
@@ -51,6 +58,18 @@ async function getRequest(hostname: string, request: string, args: { [key: strin
                             error: e
                         });
                     }
+                    
+                    let err = (json as WooCommerceSoftwareApiError);
+
+                    if (err.error !== undefined) {
+                        resolve({
+                            success: false,
+                            code: err.code,
+                            headers: res.headers,
+                            output: json,
+                            error: `Code ${err.code}: ${err.error}`
+                        });
+                    }
 
                     resolve({
                         success: true,
@@ -59,7 +78,6 @@ async function getRequest(hostname: string, request: string, args: { [key: strin
                         output: json,
                         error: "No error occurred"
                     });
-
                 });
 
             } else {
@@ -109,23 +127,17 @@ export async function checkLicense(hostname: string, product_id: string, email: 
         product_id: product_id
     });
     if (result.success) {
-        let output: WooCommerceSoftwareCheckResult = (result.output as WooCommerceSoftwareCheckResult);
-        if (!output.success) {
+        let activations: Array<WooCommerceSoftwareActivations> = (result.output as WooCommerceSoftwareCheckSuccess).activations;
+        if (activations.length == 0) {
             result.success = false;
-            result.error = `Code ${(output as WooCommerceSoftwareCheckFailed).code}: ${(output as WooCommerceSoftwareCheckFailed).error}`;
-        } else {
-            let activations: Array<WooCommerceSoftwareActivations> = (output as WooCommerceSoftwareCheckSuccess).activations;
-            if (activations.length == 0) {
-                result.success = false;
-                result.error = "Software key is not yet activated";
-            } else if (activations.find(activation => {
-                // if timestamp is 0 (no timestamp was provided), instance is ignored
-                // if platform is empty string (no platform was provided), activation_platform is ignored
-                return (timestamp == 0 || activation.instance == String(timestamp)) && (platform == "" || platform == activation.activation_platform);
-            }) === undefined) {
-                result.success = false;
-                result.error = `No matching timestamp '${timestamp}' and/or platform '${platform}'`;
-            }
+            result.error = "Software key is not yet activated";
+        } else if (activations.find(activation => {
+            // if timestamp is 0 (no timestamp was provided), instance is ignored
+            // if platform is empty string (no platform was provided), activation_platform is ignored
+            return (timestamp == 0 || activation.instance == String(timestamp)) && (platform == "" || platform == activation.activation_platform);
+        }) === undefined) {
+            result.success = false;
+            result.error = `No matching timestamp '${timestamp}' and/or platform '${platform}'`;
         }
     }
     return result;
@@ -145,25 +157,13 @@ export class WooCommerceSoftwareAddOn {
     // generate_key: https://woocommerce.com/document/software-add-on/#section-10
     async generateKey(secret_key: string, email: string | null = null, order_id: string | null = null, version: string | null = null, key_prefix: string | null = null, activations: number = 1): Promise<WooCommerceSoftwareResult> {
         let _email: string | null = email != null ? email : this.email;
-        if (_email == null) return {
-            success: false,
-            code: undefined,
-            headers: null,
-            output: null,
-            error: "No email set"
-        }
+        if (_email == null) return noEmailError;
         return generateKey(this.hostname, this.product_id, _email, secret_key, order_id, version, key_prefix, activations);
     }
 
     // activation: https://woocommerce.com/document/software-add-on/#section-11
     async activateLicense(license_key: string, instance: string | null = null, platform: string | null = null, secret_key: string | null = null): Promise<WooCommerceSoftwareResult> {
-        if (this.email == null) return {
-            success: false,
-            code: undefined,
-            headers: null,
-            output: null,
-            error: "No email set"
-        }
+        if (this.email == null) return noEmailError;
         return await this.getRequest("activation", {
             email: this.email,
             license_key: license_key,
@@ -176,13 +176,7 @@ export class WooCommerceSoftwareAddOn {
 
     // activation_reset: https://woocommerce.com/document/software-add-on/#section-12
     async resetLicense(license_key: string): Promise<WooCommerceSoftwareResult> {
-        if (this.email == null) return {
-            success: false,
-            code: undefined,
-            headers: null,
-            output: null,
-            error: "No email set"
-        }
+        if (this.email == null) return noEmailError;
         return await this.getRequest("activation_reset", {
             email: this.email,
             license_key: license_key,
@@ -192,13 +186,7 @@ export class WooCommerceSoftwareAddOn {
 
     // deactivation: https://woocommerce.com/document/software-add-on/#section-13
     async deactivateLicense(license_key: string, instance: string | null = null, activation_id: string | null = null): Promise<WooCommerceSoftwareResult> {
-        if (this.email == null) return {
-            success: false,
-            code: undefined,
-            headers: null,
-            output: null,
-            error: "No email set"
-        }
+        if (this.email == null) return noEmailError;
         return await this.getRequest("deactivation", {
             email: this.email,
             license_key: license_key,
@@ -210,13 +198,7 @@ export class WooCommerceSoftwareAddOn {
 
     // check: https://woocommerce.com/document/software-add-on/#section-14
     async checkLicense(license_key: string, timestamp: string | number = 0, platform: string = ""): Promise<WooCommerceSoftwareResult> {
-        if (this.email == null) return {
-            success: false,
-            code: undefined,
-            headers: null,
-            output: null,
-            error: "No email set"
-        }
+        if (this.email == null) return noEmailError;
         return checkLicense(this.hostname, this.product_id, this.email, license_key, timestamp, platform);
     }
 
